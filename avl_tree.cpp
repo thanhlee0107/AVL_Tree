d1664281167523343c1ffe4e90fb26fa4c30911c
vittapbay <131147129+vittapbay@users.noreply.github.com>
2023-12-02 06:46:19 +0700

    Add files via upload

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -98,10 +98,11 @@ public:
         Node*leftTree=root->pLeft;
         //left of left
         if(leftTree->balance==LH){
-            root=rotateRight(root);
+           
             root->balance=EH;
             leftTree->balance=EH;
-            taller=false;
+            taller=false; 
+            return rotateRight(root);
         }
         //right of left
         else{
@@ -112,6 +113,7 @@ public:
             }
             else if(rightTree->balance==EH){
                 leftTree->balance=EH;
+                root->balance=EH;
             }
             else{
                 root->balance=EH;
@@ -119,8 +121,8 @@ public:
             }
             rightTree->balance=EH;
             root->pLeft=rotateLeft(leftTree);
-            root=rotateRight(root);
-            taller=false;
+            taller=false; 
+            return rotateRight(root);
         }
         return root;
     }
@@ -128,10 +130,11 @@ public:
         Node*rightTree=root->pRight;
         //right of right
         if(rightTree->balance==RH){
-            root=rotateLeft(root);
             root->balance=EH;
             rightTree->balance=EH;
-            taller=false;
+            taller=false;            
+            return rotateLeft(root);
+
         }
         else{// left of right
             Node*leftTree=rightTree->pLeft;
@@ -141,15 +144,17 @@ public:
             }
             else if(leftTree->balance==EH){
                 rightTree->balance=EH;
+                root->balance=EH;
             }
             else{
                 root->balance=EH;
                 rightTree->balance=RH;
             }
+            //cout<<1<<endl;
             leftTree->balance=EH;
-            root->pRight=rotateRight(rightTree);
-            root=rotateLeft(root);
+            root->pRight=rotateRight(root->pRight);
             taller=false;
+           return rotateLeft(root);
         }
         return root;
     }
@@ -162,7 +167,7 @@ public:
             if(value<root->data){
                 root->pLeft=add(value,root->pLeft,t);
                 if (t)
-                {
+                {   
                     if(root->balance==LH){
                         root=leftBalance(root,t);
                     }
@@ -175,7 +180,7 @@ public:
                     }
                 } 
             }
-            else{
+            else{   
                     root->pRight=add(value,root->pRight,t);
                     if(t){
                         if(root->balance==LH){
@@ -186,6 +191,7 @@ public:
                             root->balance=RH;
                         }
                         else {
+                            //cout<<value<<endl;
                             root=rightBalance(root,t);
                         }
                     }
@@ -195,8 +201,7 @@ public:
     }
     void insert(const T &value){
         bool a= true;
-        this->root=add(value,this->root,a);
-        
+        this->root=add(value,this->root,a);        
     }
 int getBalance(Node*subroot){
     if(!subroot) return 0;
@@ -205,16 +210,8 @@ int getBalance(Node*subroot){
 Node* rotateLeft(Node* subroot)
 {
 //TODO: Rotate and return new root after rotate
-    //int balance=getBalance(subroot);
-    //swap
     Node*mid=subroot->pRight;
     Node*end=mid->pLeft;
-    // Node *x = y->left;                y            oxay phai
-    // Node *T2 = x->right;           x             -->   x
- //                                      T2                  y
-    // // Perform rotation                                T2
-    // x->right = y;  
-    // y->left = T2; 
    mid->pLeft=subroot;
    subroot->pRight=end;
    return mid;
@@ -227,6 +224,146 @@ Node* rotateRight(Node* subroot)
     mid->pRight=subroot;
     subroot->pLeft=end;
     return mid;
+}
+Node*deleteRightBalance(Node*&root, bool&shorter){
+    if(root->balance==LH){
+        root->balance=EH;
+    }
+    else if(root->balance==EH){
+        root->balance=RH;
+        shorter=false;
+    }
+    else{
+        Node*rightTree=root->pRight;
+        if(rightTree->balance==LH){
+            Node*leftTree=rightTree->pLeft;
+            if(leftTree->balance==LH){
+                rightTree->balance=EH;
+            }
+            else if(leftTree->balance==EH){
+                root->balance=LH;
+                rightTree->balance=EH;
+            }
+            else{
+                root->balance=LH;
+                rightTree->balance=EH;
+            }
+            leftTree->balance=EH;
+            root->pRight=rotateRight(rightTree);
+            return rotateLeft(root);
+        }
+        else{
+            if (rightTree->balance!=EH){
+                root->balance=EH;
+                rightTree->balance=EH;
+            }
+            else{
+                root->balance=RH;
+                rightTree->balance=LH;
+                shorter=false;
+            }
+            return rotateLeft(root);
+        }
+    }
+    return root;
+}
+Node*deleteLeftBalance(Node*&root, bool&shorter){
+    if(root->balance==RH){
+        root->balance=EH;
+    }
+    else if(root->balance==EH){
+        root->balance=LH;
+        shorter=false;
+    }
+    else {
+        Node*leftTree=root->pLeft;
+        if(leftTree->balance==RH){
+            Node*rightTree=leftTree->pRight;
+            if(rightTree->balance==RH){
+                leftTree->balance=LH;
+                root->balance=EH;
+            }
+            else if(rightTree->balance==EH){
+                root->balance=RH;
+                leftTree->balance=EH;
+            }
+            else{
+                root->balance=RH;
+                leftTree->balance=EH;
+            }
+            rightTree->balance=EH;
+            root->pLeft=rotateLeft(leftTree);
+            return rotateRight(root);
+        }
+        else{
+            if(leftTree->balance!=EH){
+                root->balance=EH;
+                leftTree->balance=EH;
+            }
+            else{
+                root->balance=LH;
+                leftTree->balance=RH;
+                shorter=false;
+            }
+            return rotateRight(root);
+
+        }
+    }
+    return root;
+}
+Node*deleteAVL(Node*root,T key,bool& shorter,bool& success){
+    if(!root){
+        shorter=false;
+        success=false;
+        return nullptr;
+    }
+    if(key<root->data){
+        root->pLeft=deleteAVL(root->pLeft,key,shorter,success);
+        if(shorter){
+            root=deleteRightBalance(root,shorter);
+        }
+    }
+    else if(key>root->data){
+        root->pRight=deleteAVL(root->pRight,key,shorter,success);
+        if(shorter){
+            root=deleteLeftBalance(root,shorter);
+        }
+    }
+    else{
+        Node*deleteNode=root;
+        if(!root->pRight){
+            Node*newRoot=root->pLeft;
+            success=true;
+            shorter=true;
+            //recycle deleteNode
+            delete deleteNode;
+            return newRoot;
+        }
+        else if(!root->pLeft){
+            Node*newRoot=root->pRight;
+            success=right;
+            shorter=right;
+            delete deleteNode;
+            return newRoot;
+        }
+        else{
+            Node*exchPtr=root->pLeft;
+            while(exchPtr->pRight){
+                exchPtr=exchPtr->pRight;
+            }
+            root->data=exchPtr->data;
+            root->pLeft=deleteAVL(root->pLeft,exchPtr->data,shorter,success);
+            if(shorter){
+                root=deleteRightBalance(root,shorter);
+            }
+        }
+    }
+    return root;
+}
+void remove(const T &value){
+    bool shorter=true;
+    bool success=true;
+    this->root=deleteAVL(this->root,value,shorter,success);
 }
 
     class Node
@@ -244,11 +381,14 @@ Node* rotateRight(Node* subroot)
     };
 };
 int main(){
-	
 AVLTree<int> avl;
-int nums[] = {1,2,3,4,5,6,7,8,9};
-for (int i = 0; i < 9; i++){
-    avl.insert(nums[i]);
+int arr[] = {10, 5, 15, 7};
+for (int i = 0; i < 4; i++)
+{
+  avl.insert(arr[i]);
 }
+avl.printTreeStructure();
+cout<<endl<<"------------------------------------------"<<endl;
+avl.remove(15);
 avl.printTreeStructure();
 }
